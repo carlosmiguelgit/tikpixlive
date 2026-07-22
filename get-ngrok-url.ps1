874cf59d2e5104cc8c699647a718d9e2e1a31b04
carlosmiguelgit <carlosmiguelxiaomi@gmail.com>
2026-07-22 18:09:00 -0300

    fix: server redirect /nubank -> /#/nubank

--- get-ngrok-url.ps1
+++ get-ngrok-url.ps1
@@ -4,7 +4,7 @@ while ($i -lt 10) {
     $r = Invoke-RestMethod 'http://127.0.0.1:4040/api/tunnels' -ErrorAction Stop
     $u = $r.tunnels[0].public_url
     if ($u) {
-      Write-Output ($u + '/#/nubank')
+      Write-Output ($u + '/nubank')
       exit 0
     }
   } catch {}
